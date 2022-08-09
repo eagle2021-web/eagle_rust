0d2608d16ba9d8f8c8b3c519116ce62cd7394eb5
eagle <chenwangying16@163.com>
2022-08-09 23:27:24 +0800

    mongodb_sync connect

---
+++ db/mongodb_sync/src/readme/connect.rs
@@ -0,0 +1,47 @@
+use mongodb::{
+    bson::doc,
+    sync::Client,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Book {
+    title: String,
+    author: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use mongodb::bson::doc;
+    use mongodb::sync::Client;
+    use crate::readme::connect::Book;
+
+    #[test]
+    fn test_connect() -> Result<(), mongodb::error::Error>{
+        let client = Client::with_uri_str("mongodb://localhost:27017")?;
+        let database = client.database("mydb");
+        let collection = database.collection::<Book>("books");
+
+        let docs = vec![
+            Book {
+                title: "1984".to_string(),
+                author: "George Orwell".to_string(),
+            },
+            Book {
+                title: "Animal Farm".to_string(),
+                author: "George Orwell".to_string(),
+            },
+            Book {
+                title: "The Great Gatsby".to_string(),
+                author: "F. Scott Fitzgerald".to_string(),
+            },
+        ];
+        collection.insert_many(docs, None)?;
+
+        let cursor = collection.find(doc! { "author": "George Orwell" }, None)?;
+        for result in cursor {
+            println!("title: {}", result?.title);
+        }
+        Ok(())
+    }
+}
