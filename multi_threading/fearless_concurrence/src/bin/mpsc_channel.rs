7e4cd324f99d800adcaf9e8fa388f76b119f1aec
eagle <chenwangying16@163.com>
2022-08-19 21:34:54 +0800

    mpsc_channel

--- multi_threading/fearless_concurrence/src/bin/mpsc_channel.rs
+++ multi_threading/fearless_concurrence/src/bin/mpsc_channel.rs
@@ -1,6 +1,3 @@
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
 
 fn main() {
 
@@ -38,6 +35,7 @@ mod tests {
                 String::from("for"),
                 String::from("you"),
             ];
+
 
             for val in vals {
                 tx.send(val).unwrap();
@@ -78,39 +76,39 @@ mod tests {
 
     #[test]
     fn test_mutex() {
-            let counter = Arc::new(Mutex::new(0));
-            let mut handles = vec![];
+        let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
 
-            for _ in 0..10 {
-                let counter = Arc::clone(&counter);
-                let handle = thread::spawn(move || {
-                    let mut num = counter.lock().unwrap();
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+            let handle = thread::spawn(move || {
+                let mut num = counter.lock().unwrap();
 
-                    *num += 1;
-                });
-                handles.push(handle);
-            }
+                *num += 1;
+            });
+            handles.push(handle);
+        }
 
-            for handle in handles {
-                handle.join().unwrap();
-            }
+        for handle in handles {
+            handle.join().unwrap();
+        }
 
-            println!("Result: {}", *counter.lock().unwrap());
+        println!("Result: {}", *counter.lock().unwrap());
     }
 
     #[test]
-    fn test_aa() {
+    fn test_two_channel() {
         let (tx, rx) = mpsc::channel();
-        let handle = thread::spawn(move || {
+        let _handle = thread::spawn(move || {
             for i in 0..10 {
                 tx.send(i).unwrap();
                 // println!("i = {}", i);
-                thread::sleep(Duration::from_secs(1));
+                thread::sleep(Duration::from_millis(51));
             }
         });
         let (tx2, rx2) = mpsc::channel();
 
-        let handle2 = thread::spawn(move || {
+        let _handle2 = thread::spawn(move || {
             for i in rx {
                 println!("{}", i);
                 tx2.send(i * 2).unwrap();
