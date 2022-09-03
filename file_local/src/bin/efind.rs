c5cc749b4b736af588f47b377bc92c57be24fec0
eagle <chenwangying16@163.com>
2022-09-03 12:09:22 +0800

    file_local

---
+++ file_local/src/bin/efind.rs
@@ -0,0 +1,30 @@
+use std::env;
+
+#[path = "../common/mod.rs"]
+mod common;
+
+fn main() {
+    let arg = env::args().nth(1).expect("at least one arg");
+    efind(&arg);
+}
+
+pub fn efind(arg: &str) {
+    let data = common::data::data();
+    let data_obj = data.as_object().unwrap();
+    match data_obj.get(arg) {
+        None =>  panic!("{}", format!("no key named {}", &arg)),
+        Some(a) => {
+            println!("{}", a.as_str().unwrap());
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::efind;
+
+    #[test]
+    fn test_efind() {
+        efind("hosts");
+    }
+}
