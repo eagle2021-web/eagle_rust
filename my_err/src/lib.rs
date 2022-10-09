63829136758d0f4a850001907632b7d754e0e7ea
eagle <chenwangying16@163.com>
2022-10-09 23:08:47 +0800

    EagleErr

--- my_err/src/lib.rs
+++ my_err/src/lib.rs
@@ -9,7 +9,7 @@ use failure::Fail;
 use serde_json;
 
 #[derive(Debug, Fail)]
-pub enum MyError {
+pub enum EagleErr {
     #[fail(display = "IO error {}.", _0)]
     Io(#[cause] std::io::Error),
     #[fail(display = "Parse error {}.", _0)]
@@ -18,34 +18,28 @@ pub enum MyError {
     ParseSerdeJson(#[cause] serde_json::error::Error),
 }
 
-impl From<std::io::Error> for MyError {
+impl From<std::io::Error> for EagleErr {
     fn from(error: std::io::Error) -> Self {
         println!("Error: {:?}", error);
-        MyError::Io(error)
+        EagleErr::Io(error)
     }
 }
 
-impl From<std::num::ParseIntError> for MyError {
+impl From<std::num::ParseIntError> for EagleErr {
     fn from(error: std::num::ParseIntError) -> Self {
         println!("Error: {:?}", error);
-        MyError::Parse(error)
+        EagleErr::Parse(error)
     }
 }
 
-impl From<serde_json::error::Error> for MyError {
+impl From<serde_json::error::Error> for EagleErr {
     fn from(error: serde_json::error::Error) -> Self {
         println!("Error: {:?}", error);
-        MyError::ParseSerdeJson(error)
+        EagleErr::ParseSerdeJson(error)
     }
 }
 
-
-pub fn parse_i32(contents: &str) -> Result<i32, MyError> {
-    let n = contents.trim().parse::<i32>()?;
-    Ok(2 * n)
-}
-
-fn parse_serde_json(s: &str) -> Result<serde_json::Value, MyError> {
+fn parse_serde_json(s: &str) -> Result<serde_json::Value, EagleErr> {
     let value: serde_json::Value = serde_json::from_str(s)?;
     Ok(value)
 }
@@ -57,6 +51,10 @@ mod tests {
 
     #[test]
     fn it_works() {
+        fn parse_i32(contents: &str) -> Result<i32, EagleErr> {
+            let n = contents.trim().parse::<i32>()?;
+            Ok(2 * n)
+        }
         match parse_i32("s1") {
             Ok(n) => println!("{}", n),
             Err(err) => println!("Error: {:?}", err),
@@ -65,7 +63,7 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_serde_json() -> Result<(), MyError> {
+    fn test_parse_serde_json() -> Result<(), EagleErr> {
         let s = r#"{"eagle": 18}"#;
         let res = parse_serde_json(s);
         assert!(res.is_ok());
