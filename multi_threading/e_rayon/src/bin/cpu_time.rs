9c1275d3e874eec29d98638f143ef47334a3c815
eagle <chenwangying16@163.com>
2022-11-10 22:52:03 +0800

    test_rayon

--- multi_threading/e_rayon/src/bin/cpu_time.rs
+++ multi_threading/e_rayon/src/bin/cpu_time.rs
@@ -5,11 +5,11 @@ fn main() {
     let t = get_cpu_time();
     if let Some(t) = t {
         println!("time = {}", t);
-    }else {
-
+    } else {
         panic!("acquire time err");
     }
 }
+
 pub fn get_cpu_time() -> Option<u64> {
     let (kernel, user) = unsafe {
         let process = GetCurrentProcess();
@@ -32,4 +32,37 @@ pub fn get_cpu_time() -> Option<u64> {
     let kernel = (kernel.dwHighDateTime as u64) << 32 | kernel.dwLowDateTime as u64;
     let user = (user.dwHighDateTime as u64) << 32 | user.dwLowDateTime as u64;
     Some(100 * (kernel + user))
+}
+
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_rayon() {
+        fn partition<T: PartialOrd + Send>(v: &mut [T]) ->usize {
+            let (pivot, mut i) = (v.len() - 1, 0);
+            for j in 0..pivot {
+                if v[j] < v[pivot] {
+                    v.swap(i, j);
+                    i += 1;
+                }
+            }
+            v.swap(i, pivot);
+            i
+        }
+        fn quick_sort<T: PartialOrd + Send>(v: &mut [T]) {
+            if v.len() <= 1 {
+                return;
+            }
+            let mid = partition(v);
+            let (lo, hi) = v.split_at_mut(mid);
+            rayon::join(||quick_sort(lo), ||quick_sort(hi));
+        }
+        
+        let mut v = vec![10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+        let mut v2 = v.clone();
+        quick_sort(&mut v);
+        v2.sort();
+        assert_eq!(&v, &v2);
+    }
 }
