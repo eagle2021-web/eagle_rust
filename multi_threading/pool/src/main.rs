196c9926c6dd7582fb86f13fee24da8cf5d1106c
eagle <chenwangying16@163.com>
2022-11-10 22:08:17 +0800

    pool

--- multi_threading/pool/src/main.rs
+++ multi_threading/pool/src/main.rs
@@ -5,12 +5,19 @@ use scoped_threadpool::Pool;
 fn main() {
     let mut pool = Pool::new(4);
     let mut vec = vec![0, 1, 2, 3, 4, 5, 6, 7];
+    let (mut tx, rx) = std::sync::mpsc::channel();
     pool.scoped(|scope| {
         for e in &mut vec {
+            let tx = tx.clone();
+
             scope.execute(move || {
                 *e += 1;
+                tx.send(*e);
             });
         }
     });
+    let a:i32 = rx.try_iter().collect::<Vec<i32>>().iter().sum();
+    println!("{a}");
     println!("{:?}", vec);
+    assert_eq!(a, 36);
 }
