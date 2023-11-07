c563a56aa65db806d0a42ed54c73da98555baa65
eagle2021-web <chenwangying16@163.com>
2023-11-07 23:54:23 +0800

    more print

--- my_shell/src/bin/ccc.rs
+++ my_shell/src/bin/ccc.rs
@@ -4,7 +4,9 @@ fn main() {
     let mut clipboard = Application::new().unwrap().clipboard();
     if let Some(contents) = clipboard.get_string() {
         let s = contents.as_str().replace("\\", "/");
-        println!("{}   已经在粘贴板", s);
+        println!("{} 已经在粘贴板", s);
+        println!("{}", s.replace("/", "\\\\"));
+        println!("{}", s.replace("/", "\\"));
         clipboard.put_string(s);
     }
 }
